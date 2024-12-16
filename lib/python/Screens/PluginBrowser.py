7399a56577458327e8b78df9adaa0e1c0e63f117
Littlesat <littlesat@openpli.org>
2024-12-16 18:31:23 +0100

    In PluginDownloadBrowser add expand/compress for green button
    Depending on when a expander is selected you cannot install/remove a
    plugin but instead expand or compress the expender. Correctly show this
    at the color button.
    
    (cherry picked from commit fe1e291936a61477783805a50053ac488b0d2f40)

--- lib/python/Screens/PluginBrowser.py
+++ lib/python/Screens/PluginBrowser.py
@@ -340,9 +340,9 @@ class PluginDownloadBrowser(Screen):
 		self.install_settings_name = ''
 		self.remove_settings_name = ''
 		self["text"] = Label(_("Downloading plugin information. Please wait...") if self.type == self.DOWNLOAD else _("Getting plugin information. Please wait..."))
-		self["key_blue"] = Label(_("To remove plugins") if self.type == self.DOWNLOAD else _("To download plugins"))
 		self["key_red"] = Label(_("Cancel"))
-		self["key_green"] = Label(_("Install plugin") if self.type == self.DOWNLOAD else _("Remove plugin"))
+		self["key_green"] = Label(_("Expand"))
+		self["key_blue"] = Label(_("To remove plugins") if self.type == self.DOWNLOAD else _("To download plugins"))
 		self.run = 0
 		self.remainingdata = ""
 		self["actions"] = ActionMap(["WizardActions"],
@@ -364,41 +364,14 @@ class PluginDownloadBrowser(Screen):
 			self.opkg = 'opkg'
 			self.opkg_install = 'opkg install --force-overwrite -force-defaults'
 			self.opkg_remove = self.opkg + ' remove'
-			self.opkg_toogle = self.opkg + ' flag hold'
+		self["list"].onSelectionChanged.append(self.selectionChanged)
 
-	def createPluginFilter(self):
-		#Create Plugin Filter
-		self.PLUGIN_PREFIX2 = []
-		if config.pluginfilter.drivers.getValue():
-			self.PLUGIN_PREFIX2.append(self.PLUGIN_PREFIX + 'drivers')
-		if config.pluginfilter.extensions.getValue():
-			self.PLUGIN_PREFIX2.append(self.PLUGIN_PREFIX + 'extensions')
-		if config.pluginfilter.e2_locales.getValue():
-			self.PLUGIN_PREFIX2.append('enigma2-locale-')
-		if config.pluginfilter.picons.getValue():
-			self.PLUGIN_PREFIX2.append(self.PLUGIN_PREFIX + 'picons')
-		if config.pluginfilter.pli.getValue():
-			self.PLUGIN_PREFIX2.append(self.PLUGIN_PREFIX + 'pli')
-		if config.pluginfilter.security.getValue():
-			self.PLUGIN_PREFIX2.append(self.PLUGIN_PREFIX + 'security')
-		if config.pluginfilter.settings.getValue():
-			self.PLUGIN_PREFIX2.append(self.PLUGIN_PREFIX + 'settings')
-		if config.pluginfilter.skins.getValue():
-			self.PLUGIN_PREFIX2.append(self.PLUGIN_PREFIX + 'skins')
-		if config.pluginfilter.skincomponents.getValue():
-			self.PLUGIN_PREFIX2.append(self.PLUGIN_PREFIX + 'skincomponents')
-		if config.pluginfilter.skinpacks.getValue():
-			self.PLUGIN_PREFIX2.append(self.PLUGIN_PREFIX + 'skinpacks')
-		if config.pluginfilter.softcams.getValue():
-			self.PLUGIN_PREFIX2.append(self.PLUGIN_PREFIX + 'softcams')
-		if config.pluginfilter.systemplugins.getValue():
-			self.PLUGIN_PREFIX2.append(self.PLUGIN_PREFIX + 'systemplugins')
-		if config.pluginfilter.dvb.getValue():
-			self.PLUGIN_PREFIX2.append(self.PLUGIN_PREFIX + 'dvb')
-		if config.pluginfilter.codec.getValue():
-			self.PLUGIN_PREFIX2.append(self.PLUGIN_PREFIX + 'codec')
-		if config.pluginfilter.kernel.getValue():
-			self.PLUGIN_PREFIX2.append('kernel-module-')
+	def selectionChanged(self):
+		selection = self["list"].l.getCurrentSelection()[0]
+		if isinstance(selection, str): # category
+			self["key_green"].text = _("Compress") if selection in self.expanded else _("Expand")
+		else:
+			self["key_green"].text = _("Install plugin") if self.type == self.DOWNLOAD else _("Remove plugin")
 
 	def go(self):
 		sel = self["list"].l.getCurrentSelection()
